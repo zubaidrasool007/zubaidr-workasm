138cae8aa878d6be6a155f8f16cb6c070b1e84d6
zubaidr <web.expert1221@gmail.com>
2022-08-23 15:24:31 +0500

    Worked on skills page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import { About } from "./About";
 import { Home } from "./Home";
 import { Navbar } from "./Navbar";
 import { Services } from "./Services";
+import { Skills } from "./Skills";
 
 export const Layout = ({ children }: any) => {
   return (
@@ -10,6 +11,8 @@ export const Layout = ({ children }: any) => {
       <Home />
       <About />
       <Services />
+
+      <Skills />
     </div>
   );
 };
