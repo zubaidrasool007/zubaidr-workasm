24f3502b0455a69e7b8083da755c7640be30a131
zubaidr <web.expert1221@gmail.com>
2022-08-23 12:43:44 +0500

    Worked n work on workeEducation page, added gradient line

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import { About } from "./About";
 import { Home } from "./Home";
 import { Navbar } from "./Navbar";
+import { WorkEducation } from "./WorkEducation";
 
 export const Layout = ({ children }: any) => {
   return (
@@ -8,6 +9,8 @@ export const Layout = ({ children }: any) => {
       <Navbar />
       <Home />
       <About />
+
+      <WorkEducation />
     </div>
   );
 };
