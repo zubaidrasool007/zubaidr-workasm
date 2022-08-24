44b4fd34c1f797986f79017fc30f73026f741988
zubaidr <web.expert1221@gmail.com>
2022-08-24 17:36:58 +0500

    Worked on contact page and make it responsive

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import { About } from "./About";
+import { Contact } from "./Contact";
 import { Home } from "./Home";
 import { Navbar } from "./Navbar";
 import { Services } from "./Services";
@@ -10,6 +11,7 @@ export const Layout = ({ children }: any) => {
       <Home />
       <About />
       <Services />
+      <Contact />
     </div>
   );
 };
