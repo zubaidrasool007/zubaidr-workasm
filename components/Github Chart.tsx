4d440132d7a4397dc8d28f705b7500a44d3ad637
zubaidr <web.expert1221@gmail.com>
2022-08-26 14:00:02 +0500

    Worked on git hub chart

---
+++ components/Github Chart.tsx
@@ -0,0 +1,19 @@
+export const GithubChart = () => {
+  return (
+    <div className="py-[100px]">
+      <h1 className="text-[#fff] text-[35px] font-[900] text-center mb-3 ">
+        Contributions
+      </h1>
+      <div className="about relative mb-20"></div>
+      <div className="flex justify-center ">
+        <div className="border-solid border-2 p-4">
+          <img
+            className=""
+            src="https://ghchart.rshah.org/zubaidrasool007"
+            style={{ maxWidth: 950, height: 250, width: "100%" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
